99c0e918a7bc1ba3353577365fa3e04fb245e69d
mumbuamutuku <mumbuamutuku@gmail.com>
2022-12-02 16:46:33 +0300

    pycodestyle error fixed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import sys
     argv = sys.argv[1:]
     len = len(argv)
-    count = 1 
+    count = 1
     sum = 0
     while count <= len:
         sum += int(sys.argv[count])
