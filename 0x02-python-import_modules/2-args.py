57afa67b51677bc0cc05cd5c499784e7ec2082f4
mumbuamutuku <mumbuamutuku@gmail.com>
2022-12-02 16:43:40 +0300

    pycodestyle error fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     len = len(argv)
     count = 1
-    if len is 0:
+    if len == 0:
         print("{} arguments.".format(len))
-    elif len is 1:
+    elif len == 1:
         print("{} argument:".format(len))
         print("{}: {}".format(count, sys.argv[1]))
     else:
