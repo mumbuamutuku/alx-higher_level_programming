7148c8e93deda6461d7facac40be7de015324a5f
mumbuamutuku <mumbuamutuku@gmail.com>
2022-12-05 22:52:42 +0300

    pycodestyle fxed

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,8 +3,7 @@ def max_integer(my_list=[]):
     max = my_list[0]
     if len(my_list) == 0:
         return (None)
-    else:
-        for x in my_list:
-            if x > max:
-                max = x
-        return (max)
+    for x in my_list:
+        if x > max:
+            max = x
+    return (max)
