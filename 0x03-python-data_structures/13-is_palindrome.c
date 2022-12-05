ee874d723baac2c26705beb9e90f81c3b4d1895a
mumbuamutuku <mumbuamutuku@gmail.com>
2022-12-05 22:13:51 +0300

    PALINDROME

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,2 +1,68 @@
 #include "lists.h"
 
+/**
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
+ */
+int is_palindrome(listint_t **head)
+{
+    int i = 0, j;
+    size_t count = 0;
+    listint_t *front, *rear,, *tmp;
+
+    if (*head == NULL || (*head)->next == NULL)
+        return (1);
+
+    tmp = *head;
+    while (tmp)
+    {
+        count++;
+        tmp = tmp->next;
+    }
+    tmp = *head;
+    for (j = 0; j < (count / 2) - 1; j++)
+    {
+        tmp = tmp->next;
+    }
+    if ((count % 2) == 0 && tmp->n != tmp->next->n)
+        return 0;
+
+    tmp = tmp->next->next;
+    rear = reverse_listint(&tmp);
+    front = rear;
+
+    tmp = *head;
+    while (rear)
+    {
+        if (tmp->n != rear->n)
+            return (0);
+        tmp = tmp->next;
+        rear = rear->next;
+    }
+    reverse_listint(&front);
+    return (1);
+}
---
+++ 0x03-python-data_structures/Test/6-print_matrix_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(R):
+        for j in range(C):
+            print("{}".format(matrix[i][j]))
