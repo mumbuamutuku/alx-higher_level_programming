21ab33e0cc30a5f2bc79e1e4966208dbfcc5e677
mumbuamutuku <mumbuamutuku@gmail.com>
2022-12-06 22:49:08 +0300

    check cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,11 +8,12 @@
 int check_cycle(listint_t *list)
 {
     listint_t *slow, *fast;
+    slow = fast = list;
 
     while(slow && fast && fast->next)
     {
-        slow = slow->next
-        fast = fast->next->next
+        slow = slow->next;
+        fast = fast->next->next;
         if (slow == fast)
         {
             return (1);
