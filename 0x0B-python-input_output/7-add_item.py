049544e5889c3e7c87d214d9af70c1e1585af02b
mumbuamutuku <mumbuamutuku@gmail.com>
2023-01-10 22:14:11 +0300

    0x0B. Python -Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 # 7-add_item.py
 """Add all arguments to a Python list and save them to a file."""
-import sys
+from sys import argv
+import json
+import os.path
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+my_file = 'add_item.json'
+if os.path.isfile(my_file):
+    my_list = load_from_json_file(my_file)
+else:
+    my_list = []
+count = 0
+for item in argv:
+    if count != 0:
+        my_list.append(item)
+    count += 1
+save_to_json_file(my_list, my_file)
