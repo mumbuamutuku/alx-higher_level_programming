fed42e3d3fafb2980a07e236669dfc67ded0fa8b
mumbuamutuku <mumbuamutuku@gmail.com>
2023-01-19 21:18:37 +0300

    0x0C. Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,19 +3,18 @@
 
 
 class Base:
-    """A class named Base
-
-    Represents the base for all other classes
-
+    """Represent the base model.
+    Represents the "base" for all other classes in project 0x0C*.
     Attributes:
         __nb_objects (int): The number of instantiated Bases.
-        """
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Class initialition
+        """Initialize a new Base.
         Args:
-            id (int): the identity of the new Base.
+            id (int): The identity of the new Base.
         """
         if id is not None:
             self.id = id
