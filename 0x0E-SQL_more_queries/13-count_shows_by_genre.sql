9397105c3a6337ecc49128a335caa6be42efb9da
mumbuamutuku <mumbuamutuku@gmail.com>
2023-02-16 21:46:19 +0300

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,11 @@
--- the database name will be passed as an argument of the mysql command
-
-    SELECT tv_genres.name AS genre, COUNT(*) AS 'number_shows'
-      FROM tv_genres
-INNER JOIN tv_show_genres
-        ON tv_genres.id = tv_show_genres.genre_id
-  GROUP BY genre
-  ORDER BY number_shows DESC;
+-- Lists all genres from the database hbtn_0d_tvshows along with the number of
+-- shows linked to each.
+-- Does not display genres without linked shows.
+-- Records are ordered by descending number of shows linked.
+SELECT g.name AS genre,
+       COUNT(*) AS number_of_shows
+  FROM tv_genres AS g
+       INNER JOIN tv_show_genres AS t
+       ON g.id = t.genre_id
+ GROUP BY g.name
+ ORDER BY number_of_shows DESC;
