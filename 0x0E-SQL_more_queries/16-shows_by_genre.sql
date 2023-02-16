d8b7399ef6c8e0f6e24ef768dbffaad8391f58b3
mumbuamutuku <mumbuamutuku@gmail.com>
2023-02-16 21:04:39 +0300

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- lists all shows, and all genres linked to that show,
 SELECT s.title, g.name
 FROM  tv_shows as s
-LEFT JOIN tv_show_genres as t
+LEFT JOIN tv_show_genres AS t
 ON s.id = t.show_id
-LEFT JOIN tv_genres as g
+LEFT JOIN tv_genres AS g
 ON t.genre_id = g.id
 ORDER BY s.title, g.name ASC;
