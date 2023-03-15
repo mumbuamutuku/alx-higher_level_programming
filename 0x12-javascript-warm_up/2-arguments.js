4813a423a977e63fa0645f17b74c81e9492de7e6
mumbuamutuku <mumbuamutuku@gmail.com>
2023-03-14 19:35:31 -0700

    ./2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const c = process.argv.length;
-console.log(c === 2 ? 'No argument' : c === 3 ? 'Argument found' : 'Argements found');
+console.log(c === 2 ? 'No argument' : c === 3 ? 'Argument found' : 'Arguments found');
