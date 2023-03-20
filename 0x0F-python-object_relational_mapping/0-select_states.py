d7991522e0b5b7c9ac2e031413f3848f1b9dec80
mumbuamutuku <mumbuamutuku@gmail.com>
2023-03-20 12:21:40 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
+""" Lists all states from the database hbtn_0e_0_usa.
 # Usage: ./0-select_states.py <mysql username> \
 #                             <mysql password> \
 #                             <database name>
+"""
 import sys
 import MySQLdb
 """A simple functon that lists all states from database """
