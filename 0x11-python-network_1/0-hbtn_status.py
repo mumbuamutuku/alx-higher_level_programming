c671c8ecec36b3f158ceaf47214d67af0aa8c4b7
mumbuamutuku <mumbuamutuku@gmail.com>
2023-03-31 05:08:10 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Task 0
+Fettch status
+"""
+from urllib import request
+
+if __name__ == "__main__":
+    req = request.Request('https://intranet.hbtn.io/status')
+    with request.urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- contebt: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
