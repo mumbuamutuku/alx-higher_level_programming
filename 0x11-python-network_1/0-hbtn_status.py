f7ab6ef87786c170fd7a3d43af6200f3ba62c78a
mumbuamutuku <mumbuamutuku@gmail.com>
2023-04-01 04:13:17 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-""" Task 0
-Fettch status
-"""
-from urllib import request
+"""Fetches https://intranet.hbtn.io/status."""
+import urllib.request
+
 
 if __name__ == "__main__":
-    req = request.Request('https://intranet.hbtn.io/status')
-    with request.urlopen(req) as response:
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
-        print("\t- contebt: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
