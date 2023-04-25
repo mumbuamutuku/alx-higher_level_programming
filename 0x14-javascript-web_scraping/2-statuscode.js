ebddf703c24584c4a6a109afe015b64a8aae44c8
mumbuamutuku <mumbuamutuku@gmail.com>
2023-04-25 04:49:29 -0700

    2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Write a script that display the status code of a GET request.
+
+const request = require('request');
+
+request(process.argv[2], function (error, response) {
+  if (error) {
+    console.error(error);
+  }
+  console.log('code:', response && response.statusCode);
+});
